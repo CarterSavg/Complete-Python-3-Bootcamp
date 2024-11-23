ee4c1460f0dfca971d6a875ca0c919eb7fd314ed
CarterSavg <cartersavage15@gmail.com>
2024-11-23 16:13:20 -0500

    Update blackjack.py
    cleaned up code with functions

--- 08-Milestone Project - 2/blackjack.py
+++ 08-Milestone Project - 2/blackjack.py
@@ -4,6 +4,17 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
             'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 
+class Card:
+    
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+        
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+    
+
 class Player:
     
     def __init__ (self, name = "dealer", balance = 100):
@@ -19,20 +30,20 @@ class Player:
 
     def adjust_balance(self, amount):
         self.balance += amount
+    
+    def dealer_show(self):
+        print(f"Dealer is showing {self.hand[0]}")
+
+    def show_hand(self):
+        temp = ""
+        for card in self.hand:
+            temp += card.__str__()
+        return f"You have a total of {self.hand_value} with the following cards:\n {temp}"
 
     def __str__(self):
         return f"{self.name} has a balance of: {self.balance} and {self.wins} wins"
 
-class Card:
-    
-    def __init__(self,suit,rank):
-        self.suit = suit
-        self.rank = rank
-        self.value = values[rank]
-        
-    def __str__(self):
-        return self.rank + ' of ' + self.suit
-    
+
 class Deck:
     
     def __init__(self):
@@ -51,6 +62,25 @@ class Deck:
         # Note we remove one card from the list of all_cards
         return self.all_cards.pop()
         
+def deal_hand(dealer, player):
+    deck = Deck()
+    deck.shuffle()
+
+    for i in range(0,2):
+        card = deck.all_cards.pop()
+        dealer.hand_value += card.value
+        dealer.hand.append(card)
+        card = deck.all_cards.pop()
+        player.hand_value += card.value
+        player.hand.append(card)
+
+def get_bet(bet, player):
+    valid_bet = False
+    while not valid_bet:
+        bet = input(f"Balance {player.balance}\nHow much are you betting: ")
+        if bet.isdigit() and int(bet) <= player.balance:
+            valid_bet = True
+    bet = int(bet)
 
 def main():
 
@@ -68,14 +98,11 @@ def main():
             break
 
         bet = ""
-        valid_bet = False
-        while not valid_bet:
-            bet = input(f"Balance {player.balance}\nHow much are you betting: ")
-            if bet.isdigit() and int(bet) <= player.balance:
-                valid_bet = True
-        bet = int(bet)
+        get_bet(bet, player)
 
-        deck = Deck()
-        deck.shuffle()
+        deal_hand(player = player, dealer = dealer)
+        dealer.dealer_show()
+
+        
         #TODO Create game (pull cards and let player get hit, then do the same for dealer dealer goes till they win or bust)
 main()
